520f2a517ca170bda82136354e9192072f8193ed
Zach Cervi <zachcervi@gmail.com>
2019-03-31 14:16:31 -0600

    added new list view to display categories

--- app/src/main/java/com/zachcervi/coderstore/Controller/MainActivity.kt
+++ app/src/main/java/com/zachcervi/coderstore/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.zachcervi.coderstore.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.zachcervi.coderstore.Model.Category
 import com.zachcervi.coderstore.R
+import com.zachcervi.coderstore.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var adapter : ArrayAdapter<Category
+            >
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoriesListView.adapter = adapter
     }
 }
